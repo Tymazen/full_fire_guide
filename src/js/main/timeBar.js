4e3d5b3f377c2cc86115aace70f8c391a17f49b0
Tymazen <67207109+Tymazen@users.noreply.github.com>
2024-02-26 23:10:56 -0500

    350->375

--- src/js/main/timeBar.js
+++ src/js/main/timeBar.js
@@ -8,7 +8,7 @@
     if (post && timeBar) {
         var lastScrollTop = 0;
         // Adjust the offset here
-        var offset = 350; // Adjust this value as needed
+        var offset = 375; // Adjust this value as needed
         var maxScrollTop = post.scrollHeight - offset; // Apply the offset
 
         var completed = timeBar.querySelector('.completed');
